fd526a3518258899d131f980684f540ecbe9521e
Marina Doroshenko <marina.doroshenko953@gmail.com>
2021-03-04 17:41:40 +0200

    'need_to_do'

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:lesson_4/new_box_with_favourite.dart';
-import 'package:lesson_4/news_box.dart';
 
-void main() =>  runApp(
-    new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(),
-            body: NewsBox('Новый урок по Flutter', '''В новом уроке рассказывается в каких случаях применять класс StatelessWidget и StatefulWidget. Приведены примеры их использования.''',
-                imageurl: 'https://flutter.su/favicon.png')
-            // body: NewsBoxWithFavourite('Новый урок по Flutter', '''В новом уроке рассказывается, в каких случаях применять класс StatelessWidget и StatefulWidget. Приведены примеры их использования.''',
-            //     imageurl: 'https://flutter.su/favicon.png', num: 10)
-        )
-    )
-);
+oid main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
+          title: Center (child:Text('Flutter Weather')),
+          actions:<Widget> [
+            IconButton(
+            icon: Icon(Icons.settings),
+            onPressed:(){},
+            color: Colors.black,
+            )
+          ],
+        ),
+        body:Container (
+           decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.7, 1],
+                colors: [Colors.lightBlueAccent, Colors.lightBlueAccent[100]]
+              )
+            ),
+        ),
+        floatingActionButton: FloatingActionButton(
+         child: Icon(Icons.search),
+         onPressed: (){},
+         backgroundColor: Colors.blue,
+        ),
+    ),
+    );
+  }
+}
+
+
