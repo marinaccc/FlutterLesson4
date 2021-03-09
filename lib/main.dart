2660d77dc9b79c0e39916ac1b3e0d48db4c7fbea
Marina Doroshenko <marina.doroshenko953@gmail.com>
2021-03-09 02:49:42 +0200

    all is ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-oid main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget{
           title: Center (child:Text('Flutter Weather')),
           actions:<Widget> [
             IconButton(
-            icon: Icon(Icons.settings),
-            onPressed:(){},
-            color: Colors.black,
-            )
+              icon: Icon(Icons.settings),
+              onPressed:(){},
+              color: Colors.black,
+            ),
           ],
         ),
         body:Container (
-           decoration: BoxDecoration(
+          decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
@@ -27,6 +27,11 @@ class MyApp extends StatelessWidget{
                 colors: [Colors.lightBlueAccent, Colors.lightBlueAccent[100]]
               )
             ),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(50.0),
+              child:Data('London', '2.2°C', 'Last Updated at 6:42 PM', image:'assets/images/snow.png'),
+          ),),
         ),
         floatingActionButton: FloatingActionButton(
          child: Icon(Icons.search),
@@ -38,4 +43,47 @@ class MyApp extends StatelessWidget{
   }
 }
 
+class Data extends StatelessWidget {
+  final String _city;
+  final String _temp;
+  final String _update;
+  String _image;
+
+
+  Data(this._city, this._temp, this._update, {String image}) {
+    _image = image;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_image != null && _image != '')
+              Image.asset(
+              _image,
+              width: 100.0,
+              height: 100.0,
+              fit: BoxFit.contain,
+              )
+            else
+              Container(),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+                child:Text(_city,
+                  style: TextStyle( fontFamily: 'RedHatDisplayMedium',
+                      fontSize: 30.0),),),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+            child:Text(_temp,
+              style: TextStyle(fontFamily: 'RedHatDisplayBold',
+                  fontSize: 30.0),),),
+            Text(_update,
+              style: TextStyle(fontFamily: 'RedHatDisplayRegular',
+                  fontSize: 10.0),),
+         ]),
+    );
+  }
+}
 
